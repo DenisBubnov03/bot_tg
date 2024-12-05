2b92fb0a8a4da482837823cb3ec0c67b0db4fe22
Denis Bubnov <105424941+DenisBubnov03@users.noreply.github.com>
2024-12-05 20:43:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
+from flask import Flask
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ConversationHandler
 from commands import *
 from student_management import add_student, update_student_data, get_all_students
 from notifications import check_calls, check_payments
 import os
+import threading
+
+# Переменные окружения
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# Flask приложение
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Telegram Bot is Running!"
+
 
 # Состояния для ConversationHandler
 TELEGRAM, FIO, FIO_OR_TELEGRAM, FIELD_TO_EDIT, START_DATE, COURSE_TYPE, TOTAL_PAYMENT, PAID_AMOUNT, WAIT_FOR_NEW_VALUE = range(9)
 
 
-def main():
+def start_bot():
     # Создание приложения Telegram
     application = Application.builder().token(TELEGRAM_TOKEN).build()
 
@@ -24,7 +36,7 @@ def main():
             TOTAL_PAYMENT: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_student_total_payment)],
             PAID_AMOUNT: [MessageHandler(filters.TEXT & ~filters.COMMAND, add_student_paid_amount)],
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
 
     # Обработчик редактирования студента
@@ -35,14 +47,16 @@ def main():
             FIELD_TO_EDIT: [MessageHandler(filters.TEXT & ~filters.COMMAND, edit_student_field)],
             WAIT_FOR_NEW_VALUE: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_new_value)],  # Это важно
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
+
+    # Обработчик поиска студента
     search_student_handler = ConversationHandler(
         entry_points=[MessageHandler(filters.Regex("^Поиск ученика$"), search_student)],
         states={
             FIO_OR_TELEGRAM: [MessageHandler(filters.TEXT & ~filters.COMMAND, display_student_info)],
         },
-        fallbacks=[],
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
 
     # Регистрация обработчиков
@@ -53,11 +67,11 @@ def main():
     application.add_handler(edit_student_handler)
     application.add_handler(search_student_handler)
 
-    # application.add_handler(MessageHandler(filters.Regex("Отмена"), cancel))  # Доп. проверка
-    # application.add_handler(MessageHandler(filters.ALL, debug))
-
     # Запуск бота
     application.run_polling()
 
+
+# Запуск Flask сервера и Telegram бота
 if __name__ == "__main__":
-    main()
+    threading.Thread(target=start_bot).start()
+    app.run(host="0.0.0.0", port=5000)
